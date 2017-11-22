3400c58125f91be66c345f3e7f0699ff781d4c46
grptx <robertorossetti3.14@gmail.com>
2017-11-22 10:17:22 +0100

    facebook class

--- src/Facebook.php
+++ src/Facebook.php
@@ -12,20 +12,41 @@ namespace grptx\SEO;
 use Yii;
 use yii\web\View;
 
-class Facebook extends AbstractSeo
-{
+class Facebook extends AbstractSeo {
 	public $metas = [
-		'og:locale'=>'',
-		'og:type'=>'',
-		'og:title'=>'',
+		'og:locale'           => '',
+		'og:type'             => '',
+		'og:title'            => '',
+		'og:description'      => '',
+		'og:url'              => '',
+		'og:site_name'        => '',
+		'og:updated_time'     => '',
+		'og:image'            => '',
+		'og:image:secure_url' => '',
+		'og:image:width'      => '',
+		'og:image:height'     => '',
 	];
 
-    public function render()
-    {
-	    Yii::$app->view->on(View::EVENT_BEGIN_PAGE, function(){
-			foreach ($this->metas as $key=>$value) {
-				Yii::$app->controller->view->registerMetaTag(['property'=>$key,'content'=>$value],$key);
+	public function render() {
+		if ( empty( $this->metas['og:site_name'] ) ) {
+			$this->metas['og:site_name'] = Yii::$app->name;
+		}
+		if ( empty( $this->metas['og:title'] ) ) {
+			$this->metas['og:title'] = Yii::$app->name;
+		}
+		if ( empty( $this->metas['og:url'] ) ) {
+			$this->metas['og:url'] = Yii::$app->request->absoluteUrl;
+		}
+		if ( empty( $this->metas['og:locale'] ) ) {
+			$this->metas['og:locale'] =  str_replace('-','_',Yii::$app->language);
+		}
+		Yii::$app->view->on( View::EVENT_BEGIN_PAGE, function () {
+			foreach ( $this->metas as $key => $value ) {
+				if ( empty( $value ) ) {
+					continue;
+				}
+				Yii::$app->controller->view->registerMetaTag( [ 'property' => $key, 'content' => $value ], $key );
 			}
-	    });
-    }
+		} );
+	}
 }
