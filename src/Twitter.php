b803fc918292b485a86e5ce8518d5310e0badff9
grptx <robertorossetti3.14@gmail.com>
2017-11-22 10:51:53 +0100

    twitter class fix

--- src/Twitter.php
+++ src/Twitter.php
@@ -20,18 +20,10 @@ class Twitter extends AbstractSeo {
 	];
 
 	public function render() {
-		if ( empty( $this->metas['og:site_name'] ) ) {
-			$this->metas['og:site_name'] = Yii::$app->name;
+		if ( empty( $this->metas['twitter:card'] ) ) {
+			$this->metas['twitter:card'] = 'site';
 		}
-		if ( empty( $this->metas['og:title'] ) ) {
-			$this->metas['og:title'] = Yii::$app->name;
-		}
-		if ( empty( $this->metas['og:url'] ) ) {
-			$this->metas['og:url'] = Yii::$app->request->absoluteUrl;
-		}
-		if ( empty( $this->metas['og:locale'] ) ) {
-			$this->metas['og:locale'] = str_replace( '-', '_', Yii::$app->language );
-		}
+
 		Yii::$app->view->on( View::EVENT_BEGIN_PAGE, function () {
 			foreach ( $this->metas as $key => $value ) {
 				if ( empty( $value ) ) {
